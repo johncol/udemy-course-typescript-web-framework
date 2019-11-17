6777a53cbeb20e6ee92c1f7107008e887a5670ee
John <john.19col@gmail.com>
2019-11-17 06:42:47 -0500

    Organize index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { User } from './model/User';
 import { Events } from './model/Events';
 
+const displayUserState = (user: User): void => {
+  const div: Element = document.createElement('div');
+  div.innerHTML = `
+    <h1>User changed:</h1>
+    <pre>
+      <code>${JSON.stringify(user.data, null, 2)}</code>
+    </pre>
+  `;
+  document.body.appendChild(div);
+};
+
 const user: User = User.build({ name: 'John', age: 30 });
 
-user.on(Events.change, () => {
-  const div: Element = document.createElement('div');
-  div.classList.add('code');
-  div.innerHTML = `<h1>User changed:</h1> <pre><code>${JSON.stringify(user.data, null, 2)}</code></pre>`;
-  document.body.appendChild(div);
-});
+user.on(Events.change, () => displayUserState(user));
 
-user.save().then(() => {
-  user.set({ name: 'Spartacus' });
-});
+user.save().then(() => user.set({ name: 'Spartacus' }));
