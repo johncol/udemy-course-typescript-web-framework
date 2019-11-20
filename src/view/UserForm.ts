5cca3c82edc1605ee7455dfa4e8bc38fa501f90d
John <john.19col@gmail.com>
2019-11-20 17:14:53 -0500

    Support events in the view class template

--- src/view/UserForm.ts
+++ src/view/UserForm.ts
@@ -1,7 +1,16 @@
+type EventsMap = { [key: string]: () => void };
+
 export class UserForm {
   constructor(private parent: Element) {}
 
-  template = (): string => {
+  render = (): void => {
+    const template: HTMLTemplateElement = document.createElement('template');
+    template.innerHTML = this.template();
+    this.bindEvents(template.content);
+    this.parent.append(template.content);
+  };
+
+  private template = (): string => {
     return `
       <div>
         <h1>User Form</h1>
@@ -11,9 +20,23 @@ export class UserForm {
     `;
   };
 
-  render = (): void => {
-    const template: HTMLTemplateElement = document.createElement('template');
-    template.innerHTML = this.template();
-    this.parent.append(template.content);
+  private bindEvents = (fragment: DocumentFragment): void => {
+    const eventsMap: EventsMap = this.eventsMap();
+    for (let key in eventsMap) {
+      const [event, selector] = key.split(':');
+      fragment.querySelectorAll(selector).forEach((element: Element) => {
+        element.addEventListener(event, eventsMap[key]);
+      });
+    }
+  };
+
+  private eventsMap = (): EventsMap => {
+    return {
+      'click:button': this.onButtonClick,
+    };
+  };
+
+  private onButtonClick = (): void => {
+    console.log('Button Clicked!');
   };
 }
